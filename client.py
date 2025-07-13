a6bc43995a724bb1a5f8b3e261c332f527c05466
Ryan Thomas Huff <ryanhuff@outlook.com>
2025-07-13 19:47:54 +0000

    Clinet version update

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ StreamSwarm Client - Network and system monitoring client
 """
 
 # Client version - increment when making changes
-CLIENT_VERSION = "1.9.0"
+CLIENT_VERSION = "1.9.1"
 
 import os
 import sys
