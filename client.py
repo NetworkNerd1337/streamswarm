05366af4d972d223635a512ac9f9029514d2369b
Ryan Thomas Huff <ryanhuff@outlook.com>
2025-07-13 23:01:13 +0000

    change version

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ StreamSwarm Client - Network and system monitoring client
 """
 
 # Client version - increment when making changes
-CLIENT_VERSION = "1.9.5"
+CLIENT_VERSION = "1.9.6"
 
 import os
 import sys
